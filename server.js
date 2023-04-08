6ec9dba541d05fdf92f946f8eb2b39fc646ab464
mikhail <sora@synzr.ru>
2023-04-08 22:28:23 +0500

    feat: mongoose

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 require('dotenv').config()
 
 const { Application } = require('./src/app')
+const mongoose = require('mongoose')
 
-const app = new Application(
-  process.env.NODE_ENV || 'development',
-  process.env.PORT
-    ? parseInt(process.env.PORT, 10)
-    : 5000
+mongoose.connect(
+  process.env.MONGO_URI || 'mongodb://localhost/activity-badge',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
 )
+  .then(() => {
+    console.info('[MongoDB] Connected to database.')
 
-app.start()
-  .then(() => console.info(`[Listener] Listening on port ${app.port}`))
-  .catch((error) => console.error(`[Listener] Error: ${error.message}`))
+    const app = new Application(
+      process.env.NODE_ENV || 'development',
+      process.env.PORT
+        ? parseInt(process.env.PORT, 10)
+        : 5000
+    )
+
+    app.start()
+      .then(() => console.info(`[Listener] Listening on port ${app.port}.`))
+      .catch((error) => console.error(`[Listener] Error: ${error.message}.`))
+  })
+  .catch((error) => console.error(`[MongoDB] Error: ${error.message}.`))
