547394bdaea02ff410424b29c490558d9143bb99
mikhail <sora@synzr.ru>
2023-04-08 22:39:15 +0500

    feat: cache manager

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const { Application } = require('./src/app')
+
 const mongoose = require('mongoose')
+
+const cacheManager = require('cache-manager')
+const { redisStore } = require('cache-manager-redis-yet')
 
 mongoose.connect(
   process.env.MONGO_URI || 'mongodb://localhost/activity-badge',
@@ -10,18 +14,30 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 )
-  .then(() => {
-    console.info('[MongoDB] Connected to database.')
+  .then(async () => {
+    console.info('[mongoose] Connected to MongoDB database.')
+
+    const cache = cacheManager.caching(
+      {
+        store: await redisStore(
+          {
+            url: process.env.REDIS_URL || 'redis://localhost:6379'
+          }
+        )
+      }
+    )
+    console.info('[cache manager] Connected to Redis.')
 
     const app = new Application(
       process.env.NODE_ENV || 'development',
       process.env.PORT
         ? parseInt(process.env.PORT, 10)
-        : 5000
+        : 5000,
+      cache
     )
 
     app.start()
-      .then(() => console.info(`[Listener] Listening on port ${app.port}.`))
-      .catch((error) => console.error(`[Listener] Error: ${error.message}.`))
+      .then(() => console.info(`[listener] Listening on port ${app.port}.`))
+      .catch((error) => console.error(`[listener] Error: ${error.message}.`))
   })
-  .catch((error) => console.error(`[MongoDB] Error: ${error.message}.`))
+  .catch((error) => console.error(`[server] Error: ${error.message}.`))
--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
 class Application {
-  constructor (environment = 'development', port = 5000) {
+  constructor (environment = 'development', port = 5000, cache) {
     this.environment = environment
     this.port = port
+    this.cache = cache
 
     this.expressApplication = this.#createExpressApplication()
   }
@@ -21,6 +22,8 @@ class Application {
 
     app.set('environment', this.environment)
     app.set('port', this.port)
+
+    app.set('cache', this.cache)
 
     app.get('/', (req, res) => res.send('Hello world!'))
 
